dc555d8090ab655940becd67ac6cf3312de3a0ab
Matthijspals <matthijs-pals@hotmail.com>
2025-10-30 11:32:04 +0100

    move train / init to sep files

---
+++ jaxley_example/jaxley_rnn.py
@@ -0,0 +1,176 @@
+import jaxley as jx
+from jaxley.channels import Leak, HH
+from jaxley.connect import fully_connect, connect
+from jaxley.synapses import IonotropicSynapse
+
+import numpy as np
+import jax.numpy as jnp
+
+def build_rnn(RNN_params,verbose=True):
+
+    # Define a compartment, branch, and cell
+    comp = jx.Compartment()
+    branch = jx.Branch(comp,ncomp=1)
+    cell = jx.Cell(branch, parents=[-1])
+
+    num_cells=RNN_params["n_rec"]  + RNN_params["n_out"]
+    n_rec = RNN_params["n_rec"]
+    n_inh =  RNN_params["n_inh"]
+    n_out = RNN_params["n_out"]
+    init_gain=RNN_params["init_gain"]
+    leak_out = True
+    tau = 100
+
+    net = jx.Network([cell for _ in range(num_cells)])
+    pre = net.cell(range(num_cells-n_out))
+    post = net.cell(range(num_cells-n_out))
+    readout = net.cell([range(num_cells-n_out, num_cells)])
+    #fully_connect(pre, post, IonotropicSynapse())
+    negative_inds = np.arange(n_inh).tolist()
+    rec_conn_prob=.2
+    # Connect the somas to the apical dendrites
+    for i in range(num_cells-n_out):
+        for j in range(num_cells-n_out):
+            if np.random.binomial(1, rec_conn_prob):
+                connect(
+                    net.cell(i).branch(0).comp(0),
+                    net.cell(j).branch(0).comp(0),
+                    IonotropicSynapse(),
+                )
+
+    fully_connect(pre, readout, IonotropicSynapse())
+            
+    conn_matrix = init_inh_ex_gS(
+        net,
+        negative_inds,
+        init_gain,
+        out_indices=[num_cells, num_cells + 1],
+        out_scale=RNN_params['out_scale'],
+        return_matrix=True,
+        dist="normal",
+    )
+
+
+
+    # Insert mechanisms
+    for i in range(num_cells - n_out):
+        net.cell(i).insert(HH())
+    for i in range(n_out):
+        if leak_out:
+            net.cell(num_cells - n_out + i).insert(Leak())
+            #net.cell(num_cells - 1).set("Leak_gl", 1 / (1000 * tau))
+            #net.cell(num_cells - 1).set("Leak_el", -70)
+            net.cell(num_cells - n_out + i).set("Leak_gLeak", 3e-2)
+            print("Leak out")
+        else:
+            net.cell(num_cells - n_out + i).insert(HH())
+            print("HH out")
+
+
+
+    negative_inds = np.arange(n_inh).tolist()
+    conn_matrix = init_inh_ex_gS(net, negative_inds, init_gain, return_matrix=True, verbose=verbose)
+
+    # Set some parameters
+    net.set("IonotropicSynapse_k_minus", RNN_params["k_minus"]) 
+
+    net.set("v", -67.0)
+    net.init_states()
+
+
+    n_inp = RNN_params['n_stim'] + 1 #stimuli and fixation
+    input_weights = abs(np.random.uniform(0, 1, size = (n_rec,n_inp))) * RNN_params['inp_scale']
+    in_conn_prob= RNN_params['in_conn_prob']
+    if in_conn_prob < 1.0:
+        input_weights_mask = np.zeros((n_rec,n_inp))
+        for i in range(n_inp):
+            n_nonzero = int(n_rec*in_conn_prob)
+            nonzero_indices = np.random.choice(np.arange(n_rec), size=n_nonzero, replace=False)
+            input_weights_mask[nonzero_indices, i] = 1
+    else:
+        input_weights_mask = np.ones((n_rec,n_inp))
+    input_weights = [{"input_weights": jnp.asarray(input_weights)}]
+
+
+    return net, conn_matrix, input_weights,input_weights_mask
+
+
+def init_inh_ex_gS(
+    network, inh_indices, gain, out_indices = [],e_syn_inh=-75, e_syn_ex=0, out_scale=1, return_matrix=False,dist = "normal",name = "IonotropicSynapse",verbose = True):
+    """
+    Initialises an excitatory inhibitory network with random weights, such that EI balance is preserved
+    """
+
+    n_units = (
+        max(
+            np.max(network.edges["pre_index"]),
+            np.max(network.edges["post_index"]),
+        )
+        + 1
+    )
+    if verbose:
+        print(n_units,out_indices,inh_indices)
+    n_out = len(out_indices)
+    n_units_rec = n_units-n_out
+    n_inh = len(inh_indices)
+    rec_inds = [ind for ind in range(n_units) if ind not in out_indices]
+    recurrent_post_indices = [ind for ind in network.edges["post_index"] if ind not in out_indices]
+    n_conn_rec = len(recurrent_post_indices)
+    n_conn = len(network.edges["pre_index"])
+
+    average_in_rec = n_conn_rec / n_units_rec
+    #print(n_inh,n_units_rec)
+    p_inh = n_inh / n_units_rec
+    n_ex = n_units_rec - n_inh
+    p_rec = average_in_rec / n_units_rec
+    conn_matrix = np.zeros((n_units, n_units))
+    EIratio = (1 - p_inh) / (p_inh)
+    normaliser = np.sqrt((1 / (1 - (2 * p_rec) / np.pi)) / EIratio)
+
+    if verbose: 
+        print(average_in_rec)
+        print(n_conn_rec)
+        print(n_units_rec)
+        print("conn probability recurrence: " + str(p_rec))
+        print("EIratio:" + str(EIratio))
+        print("Normaliser: " + str(normaliser))
+    # this normaliser scales the variances of the two half normal distributions to be gain**2 / N
+
+    for i in range(n_conn):
+        pre = int(network.edges.iloc[i]["pre_index"])
+        post = int(network.edges.iloc[i]["post_index"])
+        if dist=="normal":
+            samp = abs(np.random.normal(0, 1, 1)[0])
+        elif dist =="uniform":
+            samp = abs(np.random.uniform(0, 1))
+        else:
+            print("dist not recognised, use normal or uniform")
+        if post in out_indices:
+            samp*=out_scale
+        # USE this:
+        #net.select(edges=[0, 1]).set("IonotropicSynapse_gS", 0.0004)  # nS
+
+        if pre in inh_indices:
+            w = samp * normaliser * gain * EIratio / np.sqrt(average_in_rec)
+            network.edges.iloc[
+                i, network.edges.columns.get_loc(name+"_gS")
+            ] = w
+            network.edges.iloc[i, network.edges.columns.get_loc(name+"_e_syn")] = e_syn_inh
+            conn_matrix[pre, post] = w * -1
+
+        else:
+            w = samp * normaliser * gain / np.sqrt(average_in_rec)
+            network.edges.iloc[
+                i, network.edges.columns.get_loc(name+"_gS")
+            ] = w
+            network.edges.iloc[i, network.edges.columns.get_loc(name+"_e_syn")] = e_syn_ex
+            conn_matrix[pre, post] = w
+
+    ev = np.linalg.eigvals(conn_matrix[rec_inds][:,rec_inds])
+    if verbose:
+        print("Spectral radius recurrence: " + str(np.max(np.abs(ev))))
+        print("Expected gain: " + str(gain))
+    # this should be close to gain!
+
+    if return_matrix:
+        return conn_matrix
---
+++ jaxley_example/train.py
@@ -0,0 +1,112 @@
+import numpy as np
+import jaxley as jx
+import jax.numpy as jnp
+from jax import vmap, jit, value_and_grad
+import optax
+
+def simulate(params, stim, input_weights_mask, net, dt=0.025, levels=3):
+    """run simulation given stimuli"""
+    input_weights = params[0]["input_weights"]*input_weights_mask
+    syn_weights = params[1:]
+
+    data_stimuli = None
+    for i, w in zip(range(input_weights.shape[0]), input_weights):
+        data_stimuli= net.cell(i).data_stimulate(
+            jnp.inner(stim,w), data_stimuli=data_stimuli
+        )
+    num_timesteps = stim.shape[0]
+    checkpoints = [int(np.ceil(num_timesteps ** (1/levels))) for _ in range(levels)]
+    v = jx.integrate(
+        net,
+        delta_t=dt,
+        params=syn_weights,
+        data_stimuli=data_stimuli,
+        solver="bwd_euler",
+        checkpoint_lengths=checkpoints,
+    )
+    return v
+
+def predict(opt_params, stim):
+    """extract prediction (readout units activation)"""
+    v = simulate(opt_params, stim)
+    n_classes = stim.shape[1] - 1  # last input is fixation
+    return ((v[-n_classes:])).T
+
+
+def softmax(x, axis=1, eps=1e-8):
+    """Numerically stable softmax over classes (axis=0)."""
+    x_max = jnp.max(x, axis=axis, keepdims=True)
+    e_x = jnp.exp(x - x_max)
+    e_x /= jnp.sum(e_x, axis=axis, keepdims=True)
+    return jnp.clip(e_x, eps, 1 - eps)
+
+
+def ce(pred, label, mask):
+    """
+    Cross-entropy loss for one-hot labels (single trial).
+    Args:
+        pred:  (T, n_classes)
+        label: (T, n_classes)
+        mask:  (T,1)
+    """
+    probs = softmax(pred, axis=1)  # softmax over classes at each time
+    log_likelihood = jnp.sum(label * jnp.log(probs), axis=1)  # (T,)
+    mask = mask.squeeze(-1)
+    loss = -jnp.sum(mask * log_likelihood) / jnp.sum(mask)
+    return loss
+
+
+def accuracy(pred, label, mask):
+    """
+    Compute accuracy for a single trial.
+    Args:
+        pred:  (T, n_classes)
+        label: (T, n_classes)
+        mask:  (T, 1)
+    """
+    probs = softmax(pred, axis=1)
+    pred_class = jnp.argmax(probs, axis=1)   # (T,)
+    true_class = jnp.argmax(label, axis=1)   # (T,)
+
+    mask = mask.squeeze(-1)
+    correct = (pred_class == true_class) * mask
+    acc = jnp.sum(correct) / jnp.sum(mask)
+    return acc
+
+def loss_fn(opt_params, stim, label, mask, tf):
+    """Compute loss and accuracy for optimization.
+    Args:
+        opt_params: parameters in the transformed space
+        stim:  (T-1, n_classes+1)
+        label: (T, n_classses)
+        mask:  (T, 1)
+    """
+#
+
+    params = tf.forward(opt_params)
+    pred = predict(params, stim)
+    loss_val = ce(pred, label, mask)
+    acc_val = accuracy(pred, label, mask)
+    return loss_val, acc_val
+
+
+
+# initialise optimizer
+def init_opt(opt_params, lr, lr_end):
+
+    # Exponential decay of the learning rate.
+    scheduler = optax.exponential_decay(
+        init_value=lr,
+        transition_steps=1,
+        decay_rate=0.995,
+        end_value=lr_end)
+
+    # Combining gradient transforms using `optax.chain`.
+    gradient_transform = optax.chain(
+        optax.clip_by_global_norm(1.0),  # Clip by the gradient by the global norm.
+        optax.scale_by_adam(),  # Use the updates from adam.
+        optax.scale_by_schedule(scheduler),  # Use the learning rate from the scheduler.
+        optax.scale(-1.0)
+    )
+    opt_state = gradient_transform.init(opt_params)
+    return opt_state, gradient_transform
